1b89a27c24e3aa090cf6ef60dfe70725996b3894
vighakos <vighakos@turr.hu>
2021-12-09 09:38:39 +0100

    python nyerőszámok

--- lekeres.py
+++ lekeres.py
@@ -1,6 +1,10 @@
 from os import system
 import csv
+import statistics
+from statistics import mode
 system("cls")
+
+# fájl beolvasása
 
 file = open("otos.csv")
 csvreader = csv.reader(file)
@@ -8,8 +12,14 @@ csvreader = csv.reader(file)
 sorok = []
 for sor in csvreader:
     sorok.append(sor)
-
 file.close()
+
+# lista feltöltése a nyerőszámokkal
+
+nyeroszamok = []
+
+for i in range(len(sorok)):
+    nyeroszamok.append(str(sorok[i]).split(";")[-5:])
 
 # múlt heti sorsolások
 
@@ -19,11 +29,3 @@ multheti_szamok_sor_2 = str(sorok[1]).split(";")
 print(f"Múlt heti nyerőszámok: {multheti_szamok_sor_1[-5:]}")
 print(f"Előző heti nyerőszámok: {multheti_szamok_sor_2[-5:]}")
 
-# leggyakoribb számok
-
-egysor = []
-
-for i in range(len(sorok)):
-    egysor = str(sorok[i]).split(";")[-5:]
-
-sorted()
